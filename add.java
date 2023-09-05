6377e17d9a179b42b7f56008df7ec4a6e965f344
Shreyas Patil <143482147+codersofsies@users.noreply.github.com>
2023-09-05 12:03:34 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+ 
+
+public class add {
+
+
+    public static void main(String[] args) {
+
+
+        int x = 89; 
+
+
+        int y = 43;
+
+
+        int sum = x + y;
+
+
+        System.out.println("Sum is " + sum);
+
+    }
+
+}
